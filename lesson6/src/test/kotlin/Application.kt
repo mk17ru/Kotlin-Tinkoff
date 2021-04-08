4c30c71d5f879f6c7d2620b46d48581899de13b3
mk17ru <mk17.ru@mail.ru>
2021-04-08 20:56:35 +0500

    add

---
+++ lesson6/src/test/kotlin/Application.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+    
+}
+
+fun printTaskName(num : Int, text : String)  {
+    println("------------------------------------")
+    println("$num task:")
+    println(text)
+    println("Solution:")
+}
+
