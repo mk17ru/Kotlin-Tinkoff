4b548a378274dbe5dd789e397eb26735501558ab
mk17ru <mk17.ru@mail.ru>
2021-04-27 21:15:10 +0500

    add

---
+++ lesson7/src/main/kotlin/Consumer.kt
@@ -0,0 +1,16 @@
+class Consumer(private val DataBase : DataBase) {
+    fun get() : String? {
+        while(!DataBase.endingReading) {
+            synchronized(this) {
+                if (DataBase.data != null) {
+                    val d = DataBase.data
+                    DataBase.data = null
+                    return d
+                }
+            }
+        }
+
+        return null
+    }
+
+}
