465edd94f7fd910f16b0a51552b9ee2f6febc167
mk17ru <mk17.ru@mail.ru>
2021-04-27 21:49:35 +0500

    add

---
+++ lesson13/src/test/kotlin/com/lesson13/lesson13/LessonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lesson13.lesson13
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LessonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
