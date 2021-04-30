94b1d86e5b064111ec60ff6536d616edd09f1532
mk17ru <mk17.ru@mail.ru>
2021-04-30 16:16:59 +0500

    del

--- lesson/src/main/kotlin/lesson9/lesson/domain/City.kt
+++
@@ -1,8 +0,0 @@
-package lesson9.lesson.domain
-
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-
-@Entity
-class City(@Id @GeneratedValue var id: Long=0, var name: String="")
--- lesson/src/main/kotlin/lesson9/lesson/domain/User.kt
+++
@@ -1,9 +0,0 @@
-package lesson9.lesson.domain
-
-import javax.persistence.*
-
-@Entity
-class User(@Id @GeneratedValue var id: Long? = null,
-    var login: String ="",
-    var firstname: String="",
-    var lastname: String="", var cityId: Long=0)
